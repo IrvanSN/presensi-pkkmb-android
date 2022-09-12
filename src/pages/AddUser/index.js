619ae71e36fe915cef77cebccb002ab99386f558
Fahmi Pradana <fahmipradama@gmail.com>
2022-09-12 14:26:53 +0700

    add user page with add user

--- src/pages/AddUser/index.js
+++ src/pages/AddUser/index.js
@@ -1,9 +1,23 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
 import {useFonts} from 'expo-font';
+import {API_HOST} from '../../config';
+import {
+  ActionButton,
+  Loading,
+  TextInputComponent,
+  NavigatorTab,
+} from '../../components';
+import Axios from 'axios';
 import * as SplashScreen from 'expo-splash-screen';
-import {Text, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {showToast, storeData} from '../../utils';
 
 const AddUser = () => {
+  const [name, setName] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const [fontsLoaded] = useFonts({
     'Montserrat-Regular': require('../../assets/fonts/Montserrat-Regular.ttf'),
     'Montserrat-Medium': require('../../assets/fonts/Montserrat-Medium.ttf'),
@@ -30,11 +44,100 @@ const AddUser = () => {
     return null;
   }
 
+  const onSubmit = () => {
+    console.log(username, password, confirmPassword);
+    setIsLoading(true);
+    const data = {
+      name,
+      username,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setIsLoading(false);
+      showToast('Passsword yang anda masukkan tidak sama', 'danger');
+    }
+
+    if (username && password) {
+      Axios.post(`${API_HOST.url}/kafas/add`, data)
+        .then(r => {
+          setIsLoading(false);
+          showToast('${name} Telah Terdaftar', 'success');
+        })
+        .catch(e => {
+          setIsLoading(false);
+          showToast(e.response.data.message, 'danger');
+        });
+    } else {
+      setIsLoading(false);
+      showToast('Field masih kosong', 'danger');
+    }
+  };
+
   return (
-    <View onLayout={onLayoutRootView}>
-      <Text>Hello</Text>
-    </View>
+    <>
+      <View style={styles.wrapper} onLayout={onLayoutRootView}>
+        <ScrollView style={styles.scrollViewStyles}>
+          <View style={styles.navigatorWrapper}>
+            <NavigatorTab
+              date="Selasa, 27 September"
+              title="Presensi Manual"
+              navigateTo="Dashboard"
+            />
+          </View>
+          <View style={styles.warpperFormSignIn}>
+            <TextInputComponent
+              placeholder="Masukkan Nama"
+              isPasswordInput={false}
+              type="username"
+              value={name}
+              onChangeText={value => setName(value)}
+            />
+            <View style={{marginTop: 7}} />
+            <TextInputComponent
+              placeholder="Masukkan Username"
+              isPasswordInput={false}
+              type="username"
+              value={username}
+              onChangeText={value => setUsername(value)}
+            />
+            <View style={{marginTop: 7}} />
+            <TextInputComponent
+              placeholder="Masukkan Password"
+              isPassword={true}
+              type="password"
+              value={password}
+              onChangeText={value => setPassword(value)}
+            />
+            <View style={{marginTop: 7}} />
+            <TextInputComponent
+              placeholder="Konfirmasi Password"
+              isPassword={true}
+              type="password"
+              value={confirmPassword}
+              onChangeText={value => setConfirmPassword(value)}
+            />
+            <ActionButton onPress={onSubmit} />
+          </View>
+        </ScrollView>
+        {isLoading && <Loading />}
+      </View>
+    </>
   );
 };
 
 export default AddUser;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  navigatorWrapper: {
+    paddingHorizontal: 15,
+  },
+  warpperFormSignIn: {
+    marginTop: 57,
+    marginHorizontal: 17,
+  },
+});
--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -1,10 +1,15 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {useFonts} from 'expo-font';
-import {NavigatorTab} from '../../components';
+import {ActionButton, NavigatorTab, TextInputComponent} from '../../components';
+import {ScrollView, ToastAndroid} from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import {StyleSheet, View} from 'react-native';
 
 const History = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const [fontsLoaded] = useFonts({
     'Montserrat-Regular': require('../../assets/fonts/Montserrat-Regular.ttf'),
     'Montserrat-Medium': require('../../assets/fonts/Montserrat-Medium.ttf'),
@@ -31,16 +36,61 @@ const History = () => {
     return null;
   }
 
+  const onSubmit = () => {
+    setIsLoading(true);
+    const data = {
+      username,
+      password,
+    };
+
+    if (password != confirmPassword) {
+      ToastAndroid.showWithGravity(
+        'Passsword yang anda masukkan tidak sama',
+        ToastAndroid.SHORT,
+        ToastAndroid.TOP,
+      );
+      return History;
+    }
+  };
+
   return (
     <>
       <View style={styles.wrapper} onLayout={onLayoutRootView}>
-        <View style={styles.navigatorWrapper}>
-          <NavigatorTab
-            date="Selasa, 27 September"
-            title="Presensi Manual"
-            navigateTo="Dashboard"
-          />
-        </View>
+        <ScrollView style={styles.ScrollViewStyles}>
+          <View style={styles.warpperFormSignIn}>
+            <TextInputComponent
+              placeholder="Masukkan Username"
+              isPasswordInput={false}
+              type="username"
+              value={username}
+              onChangeText={value => setUsername()}
+            />
+            <View style={{marginTop:7}}/>
+            <TextInputComponent
+              placeholder="Masukkan Password"
+              isPassword={true}
+              type="password"
+              value={password}
+              onChangeText={value => setPassword()}
+            />
+            <View style={{marginTop:7}}/>
+            <TextInputComponent
+              placeholder="Konfirmasi Password"
+              isPassword={true}
+              type="password"
+              value={confirmPassword}
+              onChangeText={value => setConfirmPassword()}
+            />
+            <ActionButton onPress={onSubmit} />
+          </View>
+          <View style={styles.navigatorWrapper}>
+            <NavigatorTab
+              date="Selasa, 27 September"
+              title="Presensi Manual"
+              navigateTo="Dashboard"
+            />
+          </View>
+        </ScrollView>
       </View>
     </>
   );
